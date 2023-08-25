f467e0243db7a7e5c9842778692432f3d8f726fd
CoffezimJs <93050698+CoffezimJs@users.noreply.github.com>
2023-08-25 07:43:15 -0300

    Add files via upload

---
+++ viaproxy-termux-setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Atualiza os pacotes e instala o que é necessário
+pkg update -y 
+pkg install wget openjdk-17 
+pkg install x11-repo -y && pkg install tigervnc -y && pkg install fluxbox -y
+
+# Cria o diretório e baixa o arquivo ViaProxy
+mkdir viaproxy 
+cd viaproxy
+wget https://github.com/ViaVersion/ViaProxy/releases/download/v3.0.20/ViaProxy-3.0.20.jar
+
+# Abre o arquivo xstartup no nano
+nano ~/.vnc/xstartup <<EOF
+#!/data/data/com.termux/files/usr/bin/sh
+
+# Fluxbox desktop.
+Generate menu.
+fluxbox-generate_menu
+
+# Inicia o fluxbox.
+fluxbox &
+EOF
+
+# After exiting nano, execute the commands
+cd \$HOME/Viaproxy
+vncserver -localhost
+export DISPLAY=:0
+fluxbox &
+echo Next, execute: java -jar ViaProxy
