57f5db0d6cc6ee73665efbe3714a2a1afa8fa234
CoffezimJs <93050698+CoffezimJs@users.noreply.github.com>
2023-09-03 12:47:36 -0300

    Add files via upload

---
+++ cli.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+pkg update -y
+
+pkg install openjdk-17 -y
+
+pkg install wget -y
+
+mkdir viaproxy && cd viaproxy
+
+wget https://github.com/ViaVersion/ViaProxy/releases/download/v3.0.20/ViaProxy-3.0.20.jar
+
+clear
+
+wget https://raw.githubusercontent.com/CoffezimJs/viaproxy-termux-tool/main/vp.sh
+
+chmod 777 vp.sh
+
+clear
+
+./vp.sh
+
