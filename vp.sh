726423601ca9ec7b15fb04f0d01b491f5a698da0
CoffezimJs <93050698+CoffezimJs@users.noreply.github.com>
2023-11-01 13:34:51 -0300

    Update vp.sh

--- vp.sh
+++ vp.sh
@@ -15,6 +15,9 @@ read use_legacy
 echo "Use online mode? Y/n"
 read use_online_mode
 
+echo "Use authentication? Y/n"
+read use_auth
+
 java_command="java -jar ViaProxy-3.0.21.jar --bind_port 25568 --target_ip $target_ip --target_port $target_port --version $version"
 
 if [ "$use_legacy" == "Y" ] || [ "$use_legacy" == "y" ]; then
@@ -23,6 +26,17 @@ fi
 
 if [ "$use_online_mode" == "Y" ] || [ "$use_online_mode" == "y" ]; then
     java_command="$java_command --online_mode"
+fi
+
+if [ "$use_auth" == "Y" ] || [ "$use_auth" == "y" ]; then
+    echo "Enter authentication value: Y/n"
+    read auth_value
+
+    if [ "$auth_value" == "Y" ] || [ "$auth_value" == "y" ]; then
+        java_command="$java_command --oam_auth put _use auth y"
+    else
+        java_command="$java_command --oam_auth put _use auth n"
+    fi
 fi
 
 $java_command
