97b7b32c943608bd64cc6901b08ec142efa48c25
CoffezimJs <93050698+CoffezimJs@users.noreply.github.com>
2024-07-18 18:51:06 -0300

    Atualizar o vp.sh

--- vp.sh
+++ vp.sh
@@ -15,25 +15,20 @@ read use_online_mode
 echo "Use authentication? Y/n"
 read use_auth
 
-java_command="java -jar viaproxy.jar --bind_address 0.0.0.0:25568 --target-address $target_ip --target-version $version"
+java_command="java -jar viaproxy.jar cli --bind-address 0.0.0.0:25568 --target-a>
 
 if [ "$use_legacy" == "Y" ] || [ "$use_legacy" == "y" ]; then
-    java_command="$java_command --legacy_passthrough"
+    java_command="$java_command --allow-legacy-client-passthrough true"
 fi
 
 if [ "$use_online_mode" == "Y" ] || [ "$use_online_mode" == "y" ]; then
-    java_command="$java_command --online_mode"
+    java_command="$java_command --proxy-online-mode true"
 fi
 
 if [ "$use_auth" == "Y" ] || [ "$use_auth" == "y" ]; then
-    echo "Enter authentication value: Y/n"
-    read auth_value
-
-    if [ "$auth_value" == "Y" ] || [ "$auth_value" == "y" ]; then
-        java_command="$java_command --oam_auth put _use auth y"
-    else
-        java_command="$java_command --oam_auth put _use auth n"
-    fi
+    echo "Enter authentication method (NONE, OAM, etc.):"
+    read auth_method
+    java_command="$java_command --auth-method $auth_method"
 fi
 
 $java_command
